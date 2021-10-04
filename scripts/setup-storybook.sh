0968bfe39bea3ea637e4af70e56da884adc49f45
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-10-04 19:42:23 -0400

    storybook: rough in
    storybook: fix issue with subnav story
    
    storybook: add convenience script for deploy via fork
    
    storybook: fix branch sync script
    
    storybook: improve stories for tabs

---
+++ scripts/setup-storybook.sh
@@ -0,0 +1,17 @@
+# 1. rebase zs.storybook on the working branch,
+#    dropping any previous "run setup-storybook" commits.
+# 2. run this script and commit as "run setup-storybook".
+
+# install storybook deps
+npm i @babel/core@7 @storybook/addon-actions@6 @storybook/addon-docs@6 @storybook/addon-essentials@6 @storybook/addon-links@6 @storybook/react@6 babel-loader@8
+
+# define new storybook-specific npm scripts, specifically:
+# "storybook": "start-storybook -p 6006",
+# "build-storybook": "build-storybook"
+sed -i '' "s/\"prepare\": \"simple-git-hooks\"/\"prepare\": \"simple-git-hooks\",\n    \"storybook\": \"start-storybook -p 6006\",\n    \"build-storybook\": \"build-storybook\"/" package.json
+
+# add storybook-static to gitignore
+echo "\nstorybook-static" >>.gitignore
+
+# commit
+git commit -am "storybook: setup-storybook"
